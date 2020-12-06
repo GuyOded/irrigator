60e094ed4d3b90058f83e74fa6ad1a76ac0a1553
Ofer Oded <ooded@apple.com>
2020-12-06 19:25:18 +0200

    Change baudrate and change main to support last change to MoistureSensor

--- src/main.ino
+++ src/main.ino
@@ -2,19 +2,18 @@
 
 const unsigned int LED = 13;
 
-MoistureSensor moistureSensor = MoistureSensor(0);
+MoistureSensor moistureSensor = MoistureSensor(A0);
 
 void setup()
 {
     pinMode(LED, OUTPUT);
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 void loop()
 {
     int moistureVal;
-    moistureVal = analogRead(0); //connect sensor to Analog 0
-    Serial.println(moistureVal);
+    moistureVal = moistureSensor.readValue();
     Serial.println(moistureSensor.isDry(moistureVal) ? "dry" : moistureSensor.isWater(moistureVal) ? "water" : moistureSensor.isWet(moistureVal) ? "wet" : "not defined");
     delay(500);
 }
